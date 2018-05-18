4d42ade8fa3247d81ce04409c3ce408820dbef16
Carlos Villanueva Ousset <crvo84@gmail.com>
2018-05-17 21:15:06 -0500

    Organize project.

---
+++ MyList/Model/BindableType.swift
@@ -0,0 +1,10 @@
+//
+//  BindableType.swift
+//  MyList
+//
+//  Created by Carlos Villanueva Ousset on 5/17/18.
+//  Copyright © 2018 Carlos Villanueva Ousset. All rights reserved.
+//
+
+import Foundation
+//import
